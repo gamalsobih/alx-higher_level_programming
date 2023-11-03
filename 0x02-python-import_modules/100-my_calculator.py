5035e99849cc3a1bb66a6529088fc247ae15f1ee
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-11-03 05:48:51 +0200

    Create 100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+  if len(len(sys.argv)) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b> ")
+    exit()
+  a = int(sys.argv[1])
+  b = int(sys.argv[3])
+  c = sys.argv[2]
+  if c == "+":
+    r = a + b
+    print("{:d} + {:d} = {:d}".format(a, b, r))
+  elif c == "-":
+    r = a - b
+    print("{:d} - {:d} = {:d}".format(a, b, r))
+  elif c == "*":
+    r = a * b
+    print("{:d} * {:d} = {:d}".format(a, b, r))
+  elif c == "/":
+    r = a / b
+    print("{:d} / {:d} = {:d}".format(a, b, r))
+  else:
+    print("Unknown operator. Available operators: +, -, * and / ")

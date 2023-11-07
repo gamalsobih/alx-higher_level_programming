70a1e4cfd50019ccbb3725dfac841d56b1a2f49b
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-11-07 04:04:51 +0200

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        my_tuple = (0, None)
-    else:
-        my_tuple = (len(sentence), sentence[:1])
-    return(my_tuple)
+    if isinstance(sentence, str):
+        count = len(sentence)
+        l_sentence = list(sentence)
+        if len(l_sentence) >= 1:
+            res_tuple = (count, l_sentence[0])
+        else:
+            res_tuple = (count, None)
+
+        return res_tuple

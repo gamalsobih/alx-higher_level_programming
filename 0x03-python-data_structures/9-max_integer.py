476870def2e84c40eafab7f164e81cd789ed7e51
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-11-07 04:05:23 +0200

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == []:
-        return(None)
-    my_list.sort()
-    return(my_list[-1])
+    if isinstance(my_list, list):
+        if len(my_list) == 0:
+            return None
+        sort_list = sorted(my_list)
+        big = sort_list[len(my_list) - 1]
+
+        return big

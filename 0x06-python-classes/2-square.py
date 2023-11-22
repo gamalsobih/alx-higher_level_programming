61be45b0ab12dc63f582d32ff3d09679577ba653
gamal sob <133385330+gamalsobih@users.noreply.github.com>
2023-11-22 02:23:05 +0200

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
+"""Defines a class Square."""
 
 
 class Square:
-    """
-    class square that has attributes:
-        size
-    some attributes are protected from input.
-    """
+    """Represents a square."""
+
     def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
         """
-        the initialization function for the square class
-        checks for input errors for size
-        """
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
